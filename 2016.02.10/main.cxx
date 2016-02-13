16c4e2ddf3b0a9c57053a62c5298cf8184da45dd
Damian Reynolds <damian.reynolds@triangular-logic.com>
2016-02-13 01:11:49 -0500

    Permutations work

--- 2016.02.10/main.cxx
+++ 2016.02.10/main.cxx
@@ -8,6 +8,8 @@ using std::plus;
 using std::minus;
 using std::multiplies;
 using std::divides;
+#include <iterator>
+using std::advance;
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -15,32 +17,47 @@ using std::endl;
 using std::list;
 
 
-int       Head  (const list<int>& input);
-list<int> Tail  (const list<int>& input);
-void      Print (const list<int>& input);
+void Print (const list<int>& input);
+void Permute(const list<int>& input);
+void Permute(const list<int>& head, const list<int>& tail);
 
 int main()
 {
     list<int> input({ 25, 50, 75, 100, 3, 6 });
     Print(input);
+    Permute(input);
     
     return 0;
-}
-
-int Head(const list<int>& input)
-{
-    return input.front();
-}
-
-list<int> Tail(const list<int>& input)
-{
-    list<int> output;
-    if (input.size() > 2) output.assign(++input.begin(), input.end());
-    return output;
 }
 
 void Print(const list<int>& input)
 {
     for (const int& i : input) cout << i << " ";
     cout << endl;
+}
+
+void Permute(const list<int>& input)
+{
+    list<int> head;
+    Permute(head, input);
+}
+
+void Permute(const list<int>& head, const list<int>& tail)
+{
+    if (tail.empty())
+    {
+        Print(head);
+        return;
+    }
+    size_t size = tail.size();
+    for (size_t pos = 0; pos < size; ++pos)
+    {
+        list<int> newTail = tail;
+        list<int>::const_iterator it = newTail.begin();
+        advance(it, pos);
+        list<int> newHead = head;
+        newHead.emplace_back(*it);
+        newTail.erase(it);
+        Permute(newHead, newTail);
+    }
 }
